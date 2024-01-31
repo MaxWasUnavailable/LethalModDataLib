05a881703cfe119fc5c33af2fe7a728d71f73fd7
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-01-31 05:28:54 +0100

    Add ModDataAttribute

---
+++ LethalModDataLib/Attributes/ModDataAttribute.cs
@@ -0,0 +1,39 @@
+using System;
+using LethalModDataLib.Enums;
+
+namespace LethalModDataLib.Attributes;
+
+/// <summary>
+///     Attribute to mark fields to be saved and loaded by the mod data system.
+/// </summary>
+[AttributeUsage(AttributeTargets.Field)]
+public class ModDataAttribute : Attribute
+{
+    /// <summary>
+    ///     Attribute to mark fields to be saved and loaded by the mod data system.
+    /// </summary>
+    /// <param name="saveWhen"> When to save the field. <see cref="SaveWhen" /> </param>
+    /// <param name="loadWhen"> When to load the field. <see cref="LoadWhen" /> </param>
+    /// <param name="saveLocation"> Where to save the field. <see cref="SaveLocation" /> </param>
+    /// <param name="baseKey">
+    ///     Key prefix for the field. The ModData system will automatically set this to the mod's GUID
+    ///     unless it is set manually.
+    /// </param>
+    public ModDataAttribute(SaveWhen saveWhen, LoadWhen loadWhen, SaveLocation saveLocation, string? baseKey = null)
+    {
+        SaveWhen = saveWhen;
+        LoadWhen = loadWhen;
+        SaveLocation = saveLocation;
+        BaseKey = baseKey;
+    }
+
+    public LoadWhen LoadWhen { get; }
+    public SaveWhen SaveWhen { get; }
+    public SaveLocation SaveLocation { get; }
+
+    /// <summary>
+    ///     Key prefix for the field. The ModData system will automatically set this to the mod's GUID unless it is set
+    ///     manually.
+    /// </summary>
+    public string? BaseKey { get; set; }
+}
