2c87ef775e1e76b1a6dfee632b68aa91d14b2f9d
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-01-31 06:09:43 +0100

    Add abstract ModDataContainer system

---
+++ LethalModDataLib/Base/ModDataContainer.cs
@@ -0,0 +1,108 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using LethalModDataLib.Enums;
+using LethalModDataLib.Features;
+
+namespace LethalModDataLib.Base;
+
+/// <summary>
+///     Base class for ModData containers.
+/// </summary>
+public abstract class ModDataContainer
+{
+    private SaveLocation SaveLocation { get; } = SaveLocation.CurrentSave;
+
+    /// <summary>
+    ///     Gets all fields in the container.
+    /// </summary>
+    /// <returns> All fields in the container. </returns>
+    private List<FieldInfo> GetFields()
+    {
+        var type = GetType();
+
+        // Get all fields in the container
+        var fields = type.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance |
+                                    BindingFlags.Static);
+
+        return fields.ToList();
+    }
+
+    /// <summary>
+    ///     Gets the prefix for moddata keys of fields in the container.
+    /// </summary>
+    /// <returns> The prefix for moddata keys of fields in the container. </returns>
+    private string GetPrefix()
+    {
+        var type = GetType();
+
+        return type.Assembly.GetName().Name + "." + type.Name + ".";
+    }
+
+    /// <summary>
+    ///     Saves all fields in the container.
+    /// </summary>
+    public void Save()
+    {
+        PreSave();
+
+        var prefix = GetPrefix();
+
+        foreach (var field in GetFields())
+        {
+            var value = field.GetValue(this);
+
+            ModDataHandler.SaveData(value, prefix + field.Name, SaveLocation);
+        }
+
+        PostSave();
+    }
+
+    /// <summary>
+    ///     Called before saving.
+    /// </summary>
+    internal virtual void PreSave()
+    {
+    }
+
+    /// <summary>
+    ///     Called after saving.
+    /// </summary>
+    internal virtual void PostSave()
+    {
+    }
+
+    /// <summary>
+    ///     Loads all fields in the container.
+    /// </summary>
+    public void Load()
+    {
+        PreLoad();
+
+        var prefix = GetPrefix();
+
+        foreach (var field in GetFields())
+        {
+            var value = ModDataHandler.LoadData<object>(prefix + field.Name, saveLocation: SaveLocation);
+
+            field.SetValue(this, value);
+        }
+
+        PostLoad();
+    }
+
+    /// <summary>
+    ///     Called before loading.
+    /// </summary>
+    internal virtual void PreLoad()
+    {
+    }
+
+    /// <summary>
+    ///     Called after loading.
+    /// </summary>
+    /// <remarks> This is a good place to sanity check loaded values. </remarks>
+    internal virtual void PostLoad()
+    {
+    }
+}
