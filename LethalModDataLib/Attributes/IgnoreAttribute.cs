390a497f34ffff6f19a97386d8c31880bb2a04f1
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-02-07 02:02:19 +0100

    Add IgnoreAttribute & IgnoreFlag

---
+++ LethalModDataLib/Attributes/IgnoreAttribute.cs
@@ -0,0 +1,25 @@
+using System;
+using LethalModDataLib.Enums;
+
+namespace LethalModDataLib.Attributes;
+
+/// <summary>
+///     Attribute to mark fields to be ignored by ModDataContainers' Save and Load methods.
+/// </summary>
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+public class IgnoreAttribute : Attribute
+{
+    /// <summary>
+    ///     Attribute to mark fields or properties to be ignored by ModDataContainers' Save and Load methods.
+    /// </summary>
+    /// Accept zero or more IgnoreFlag enum values.
+    public IgnoreAttribute(IgnoreFlag ignoreFlags = IgnoreFlag.None)
+    {
+        IgnoreFlags = ignoreFlags;
+    }
+    
+    /// <summary>
+    ///     Enum value to mark fields or properties to be ignored by ModDataContainers' Save and Load methods.
+    /// </summary>
+    public IgnoreFlag IgnoreFlags { get; }
+}
---
+++ LethalModDataLib/Enums/IgnoreFlag.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace LethalModDataLib.Enums;
+
+/// <summary>
+///     Enum for flags for the IgnoreAttribute.
+/// </summary>
+[Flags]
+public enum IgnoreFlag
+{
+    /// <summary>
+    ///     No flags. Completely ignore the field or property.
+    /// </summary>
+    None = 0,
+
+    /// <summary>
+    ///     Ignore the field or property when saving.
+    /// </summary>
+    OnSave = 1,
+
+    /// <summary>
+    ///     Ignore the field or property when loading.
+    /// </summary>
+    OnLoad = 2,
+
+    /// <summary>
+    ///     Ignore the field or property if it is null.
+    /// </summary>
+    IfNull = 4
+}
