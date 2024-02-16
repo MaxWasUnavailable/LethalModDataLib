bce5bf01b8b64582cff02364087d47e85963a9a7
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-02-16 15:34:37 +0100

    Expose Instance to interface

--- LethalModDataLib/Interfaces/IModDataKey.cs
+++ LethalModDataLib/Interfaces/IModDataKey.cs
@@ -9,6 +9,7 @@ public interface IModDataKey
 {
     public string Name { get; }
     public string? AssemblyQualifiedName { get; }
+    public object? Instance { get; }
     public bool TryGetValue(out object? value);
     public bool TrySetValue(object? value);
     public ModDataAttribute GetModDataAttribute();
