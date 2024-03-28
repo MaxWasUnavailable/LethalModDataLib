0177fec626bde5a197217cb626c4d000ae118de0
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-03-28 23:31:43 +0100

    Add OriginalValue property to ModDataValue

--- LethalModDataLib/Models/ModDataValue.cs
+++ LethalModDataLib/Models/ModDataValue.cs
@@ -6,7 +6,7 @@ namespace LethalModDataLib.Models;
 /// <summary>
 ///     Record used as value for ModDataFields and ModDataProperties dictionaries.
 /// </summary>
-public record ModDataValue(ModDataAttribute ModDataAttribute, string? KeySuffix = null)
+public record ModDataValue(ModDataAttribute ModDataAttribute, string? KeySuffix = null, object? OriginalValue = null)
 {
     /// <summary>
     ///     Base key for ES3.
@@ -36,4 +36,9 @@ public record ModDataValue(ModDataAttribute ModDataAttribute, string? KeySuffix 
     ///     Where to save the field.
     /// </summary>
     public SaveLocation SaveLocation => ModDataAttribute.SaveLocation;
+
+    /// <summary>
+    ///     Original value of the field.
+    /// </summary>
+    public object? OriginalValue { get; set; }
 }
